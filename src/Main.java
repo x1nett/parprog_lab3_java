7dbedb1a0477042b3a9e6c388db7f65e08278e08
x1nett <132476761+x1nett@users.noreply.github.com>
2025-05-28 22:53:34 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int storageSize = 5;
-        int[] producerItems = {5, 3, 4}; // кожен виробник виробляє певну кількість
-        int[] consumerItems = {6, 6};    // кожен споживач споживає певну кількість
+        int[] producerItems = {5, 3, 4}; 
+        int[] consumerItems = {6, 6};    
 
         Storage storage = new Storage(storageSize);
 
-        // Створення виробників
         for (int i = 0; i < producerItems.length; i++) {
             Producer producer = new Producer(i, producerItems[i], storage);
             new Thread(producer).start();
         }
 
-        // Створення споживачів
         for (int i = 0; i < consumerItems.length; i++) {
             Consumer consumer = new Consumer(i, consumerItems[i], storage);
             new Thread(consumer).start();
