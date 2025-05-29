d6a76dd236ead069c3f623dd778913f5c652e807
x1nett <132476761+x1nett@users.noreply.github.com>
2025-05-29 10:47:41 +0300

    Update Storage.java

--- src/Storage.java
+++ src/Storage.java
@@ -1,18 +1,21 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.Semaphore;
 
 public class Storage {
-    private final List<String> buffer = new ArrayList<>();
+    private final int capacity;
+    private final Queue<String> buffer = new LinkedList<>();
     private final Semaphore access = new Semaphore(1);
     private final Semaphore full;
     private final Semaphore empty;
 
     public Storage(int capacity) {
-        full = new Semaphore(capacity);
-        empty = new Semaphore(0);
+        this.capacity = capacity;
+        this.full = new Semaphore(capacity);
+        this.empty = new Semaphore(0);
     }
 
-    public void produce(String item, int producerId) throws InterruptedException {
+    public void add(String item, int producerId) throws InterruptedException {
         full.acquire();
         access.acquire();
 
@@ -23,16 +26,15 @@ public class Storage {
         empty.release();
     }
 
-    public void consume(int consumerId) throws InterruptedException {
+    public String take(int consumerId) throws InterruptedException {
         empty.acquire();
         access.acquire();
 
-        if (!buffer.isEmpty()) {
-            String item = buffer.remove(0);
-            System.out.println("\t[Consumer " + consumerId + "] Consumed: " + item);
-        }
+        String item = buffer.poll();
+        System.out.println("\t[Consumer " + consumerId + "] Consumed: " + item);
 
         access.release();
         full.release();
+        return item;
     }
 }
