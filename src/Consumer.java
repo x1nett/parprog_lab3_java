6b3a2770464e2d46a0157e4662c87f340ea89a82
x1nett <132476761+x1nett@users.noreply.github.com>
2025-05-29 10:48:54 +0300

    Update Consumer.java

--- src/Consumer.java
+++ src/Consumer.java
@@ -1,20 +1,22 @@
 public class Consumer implements Runnable {
     private final int id;
     private final int itemCount;
+    private final String[] buffer;
     private final Storage storage;
 
     public Consumer(int id, int itemCount, Storage storage) {
         this.id = id;
         this.itemCount = itemCount;
         this.storage = storage;
+        this.buffer = new String[itemCount];
     }
 
     @Override
     public void run() {
         try {
             for (int i = 0; i < itemCount; i++) {
-                storage.consume(id);
-                Thread.sleep(500); 
+                buffer[i] = storage.take(id);
+                Thread.sleep(500); // Для демонстрації
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
