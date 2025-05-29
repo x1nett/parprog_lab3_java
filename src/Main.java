c9fb47a23f9f11b0d1ba16c87a36778b359a7d99
x1nett <132476761+x1nett@users.noreply.github.com>
2025-05-29 10:49:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         int storageSize = 5;
-        int[] producerItems = {5, 3, 4}; 
-        int[] consumerItems = {6, 6};    
+
+        // Дані для виробників
+        String[][] producerData = {
+            {"A1", "A2", "A3"},
+            {"B1", "B2"},
+            {"C1", "C2", "C3", "C4"}
+        };
+        
+        int[] consumerNeeds = {4, 5};
 
         Storage storage = new Storage(storageSize);
 
-        for (int i = 0; i < producerItems.length; i++) {
-            Producer producer = new Producer(i, producerItems[i], storage);
+        for (int i = 0; i < producerData.length; i++) {
+            Producer producer = new Producer(i, producerData[i], storage);
             new Thread(producer).start();
         }
 
-        for (int i = 0; i < consumerItems.length; i++) {
-            Consumer consumer = new Consumer(i, consumerItems[i], storage);
+        // Запуск споживачів
+        for (int i = 0; i < consumerNeeds.length; i++) {
+            Consumer consumer = new Consumer(i, consumerNeeds[i], storage);
             new Thread(consumer).start();
         }
     }
